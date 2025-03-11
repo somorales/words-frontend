dee9c4dbbe4a5fb6da724f048469352e3034618b
Sofia Morales <sofimoralesroa92@gmail.com>
2025-03-11 18:11:50 +0100

    Estilos en loginPage

--- src/pages/auth/LoginPage.jsx
+++ src/pages/auth/LoginPage.jsx
@@ -5,6 +5,7 @@ import { AuthContext } from "../../context/auth.context.jsx";
 import { Link } from "react-router-dom";
 import { ToastContext } from "../../context/toast.context.jsx";
 import Loading from "../../components/Loading.jsx";
+import welcome from "../../assets/images/Welcome.png";
 
 function LoginPage() {
   const navigate = useNavigate();
@@ -16,7 +17,7 @@ function LoginPage() {
 
   useEffect(() => {
     setTimeout(() => {
-      //setIsLoading(false);
+      setIsLoading(false);
     }, 2000);
   }, []);
 
@@ -62,78 +63,69 @@ function LoginPage() {
   }
 
   return (
-    <>
-      <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8 bg-[#4D3E7F]">
-        <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
-          <form onSubmit={handleLogin} method="POST" className="space-y-6">
-            <div>
-              <div className="flex items-center justify-between">
-                <label
-                  htmlFor="email"
-                  className="block text-sm font-medium leading-6 text-[#000000]"
-                >
-                  Correo Eletrónico
-                </label>
-              </div>
-              <div className="mt-2">
-                <input
-                  onChange={handleEmailChange}
-                  id="email"
-                  name="email"
-                  type="email"
-                  placeholder="ejemplo@correo.com"
-                  required
-                  autoComplete="email"
-                  className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-[#c07c53] sm:text-sm sm:leading-6"
-                />
-              </div>
-            </div>
-
-            <div>
-              <div className="flex items-center justify-between">
-                <label
-                  htmlFor="password"
-                  className="block text-sm font-medium leading-6 text-gray-900"
-                >
-                  Contraseña
-                </label>
-              </div>
-              <div className="mt-2">
-                <input
-                  onChange={handlePasswordChange}
-                  id="password"
-                  name="password"
-                  type="password"
-                  placeholder="Usar entre 8-16 caracteres"
-                  required
-                  autoComplete="current-password"
-                  className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-[#c07c53] sm:text-sm sm:leading-6"
-                />
-              </div>
-            </div>
-
-            <div>
-              <button
-                type="submit"
-                className="flex w-full justify-center rounded-md bg-[#c07c53] px-3 py-1.5 text-sm font-semibold leading-6 text-[#efe8db] shadow-sm hover:bg-[#D68C60] focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-[#D68C60]"
-              >
-                Iniciar sesión
-              </button>
-            </div>
-          </form>
-
-          <p className=" font-light mt-10 text-center text-sm text-[#000000]">
-            No tienes cuenta?{" "}
-            <Link
-              to={`/signup`}
-              className="font-bold leading-6 text-[#c07c53] hover:text-[#D68C60]"
+    <div className="flex items-center justify-center h-screen">
+      <div className="p-4 rounded-lg  w-96 ">
+        <img src={welcome} />
+        <form onSubmit={handleLogin} className="mt-8">
+          <div className="mb-8">
+            <label
+              htmlFor="email"
+              className="block text-lg font-medium text-black mb-2"
             >
-              Crear Cuenta
+              Email address
+            </label>
+            <input
+              id="email"
+              name="email"
+              type="email"
+              placeholder="example@correo.com"
+              required
+              autoComplete="email"
+              value={email}
+              onChange={handleEmailChange}
+              className="w-full mt-1 p-2 border border-gray-300 rounded-md focus:ring-2 focus:ring-[#5B3E96]"
+            />
+          </div>
+
+          <div className="mb-8">
+            <label
+              htmlFor="password"
+              className="block text-lg font-medium text-black mb-2"
+            >
+              Password
+            </label>
+            <input
+              id="password"
+              name="password"
+              type="password"
+              placeholder="example: P@r1$234."
+              required
+              autoComplete="current-password"
+              value={password}
+              onChange={handlePasswordChange}
+              className="w-full mt-1 p-2 border border-gray-300 rounded-md focus:ring-2 focus:ring-[#5B3E96]"
+            />
+          </div>
+
+          <button
+            type="submit"
+            className="w-full bg-[#4D3E7F] text-white py-2 rounded-full text-lg font-medium hover:bg-[#47307D] transition"
+          >
+            Login
+          </button>
+          {/* Enlace para registrarse */}
+          <p className="mt-8 text-center text-lg font-medium text-[#4D3E7F]">
+            New here?{" "}
+            <Link
+              to="/signup"
+              className="font-bold text-lg  text-[##4D3E7F] hover:text-[#4D3E7F]"
+            >
+              Create an account.
             </Link>
           </p>
-        </div>
+        </form>
       </div>
-    </>
+    </div>
   );
 }
 
