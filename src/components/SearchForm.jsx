bc817c2783673f5030b624497ed0bf2abe767561
Sofia Morales <sofimoralesroa92@gmail.com>
2025-03-20 21:26:45 +0100

    se corrige css de search form

--- src/components/SearchForm.jsx
+++ src/components/SearchForm.jsx
@@ -25,7 +25,7 @@ export default function SearchForm(props) {
         />
         <button
           type="submit"
-          className="absolute inset-y-0 right-0 flex items-center px-8 text-gray-500 rounded-r-full hover:text-blue-500 focus:outline-none"
+          className="absolute inset-y-0 right-[4.5rem] flex items-center text-gray-500 rounded-r-full hover:text-blue-500 focus:outline-none"
         >
           <svg
             className="w-5 h-5"
@@ -45,50 +45,4 @@ export default function SearchForm(props) {
       </div>
     </form>
   );
-}
-
-{
-  /*}
-    <>
-      <form
-        onSubmit={handleSubmit}
-        className="flex items-center max-w-sm mx-auto"
-      >
-        <label for="simple-search" className="sr-only">
-          Buscar
-        </label>
-        <div className="relative w-full">
-          <input
-            type="text"
-            id="simple-search"
-            className="block w-full h-10 p-4 ps-10 text-sm text-gray-900 rounded-lg ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-[#c07c53] focus:border-none"
-            placeholder={props.placeholder}
-            onChange={handleSearchChange}
-          />
-        </div>
-        <button
-          type="submit"
-          className="p-2.5 ms-2 text-sm font-semibold text-[#efe8db] bg-[#c07c53] hover:bg-[#D68C60] focus:outline-none focus:ring-2 focus:ring-[#c07c53] focus:ring-offset-2 rounded-lg"
-        >
-          <svg
-            className="w-4 h-4"
-            aria-hidden="true"
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 20 20"
-          >
-            <path
-              stroke="currentColor"
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth="2"
-              d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"
-            />
-          </svg>
-          <span className="sr-only">Buscar</span>
-        </button>
-      </form>
-    </>
-  );
-} */
 }
