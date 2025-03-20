81326d43f1ea6041b1451ef7353000f4c6451822
Sofia Morales <sofimoralesroa92@gmail.com>
2025-03-20 21:38:18 +0100

    font

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,14 @@
 /** @type {import('tailwindcss').Config} */
+const defaultTheme = require("tailwindcss/defaultTheme");
 export default {
-    content: [
-      "./index.html",
-      "./src/**/*.{js,ts,jsx,tsx}",
-    ],
-    theme: {
-      extend: {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        // descomentar lo siguiente para cambiar la fuente a poppins...
+        // sans: ["Poppins", ...defaultTheme.fontFamily.sans],
       },
     },
-    plugins: [],
-  }
+  },
+  plugins: [],
+};
